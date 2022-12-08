0427585a38e21badf66aeda9b1b14729c50fc323
amanda <amanda@actualize.co>
2022-12-08 13:51:46 -0600

    uses props to pass data from home to RI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
+
 function Header() {
   return (
     <div>
       <header>
-        <a href="#">Home</a> |  <a href="#recipes-index">All Recipes</a> |  <a href="#recipes-new">New Recipe</a> |
+        <a href="#">Home</a> | <a href="#recipes-index">All Recipes</a> | <a href="#recipes-new">New Recipe</a> |
       </header>
     </div>
   );
@@ -14,10 +16,10 @@ function RecipesNew() {
       <h1>New Recipe</h1>
       <form>
         <div>
-          title: <input type="text"/>
+          title: <input type="text" />
         </div>
         <div>
-          chef: <input type="text"/>
+          chef: <input type="text" />
         </div>
         <button type="submit">Create recipe</button>
       </form>
@@ -25,25 +27,28 @@ function RecipesNew() {
   );
 }
 
-function RecipesIndex() {
+function RecipesIndex(props) {
+  console.log(props);
   return (
     <div id="recipes-index">
       <h1>All Recipes!!</h1>
-      <div className="recipes">
-        <h2>Raw Eggs</h2>
-        <img src="https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg" alt="" />
-        <p>Chef: Peter Jang</p>
-        <button>More info</button>
-      </div>
-      <div className="recipes">
-        <h2>Mud Pie</h2>
-        <img
-          src="https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg"
-          alt=""
-        />
-        <p>Chef: Jay Wengrow</p>
-        <button>More info</button>
-      </div>
+        <div className="recipes">
+          <div>
+            <h2>{props.recipes[0].title}</h2>
+            <h4>{props.recipes[0].chef}</h4>
+            <img src={props.recipes[0].image_url} />
+          </div>
+          <div>
+            <h2>{props.recipes[1].title}</h2>
+            <h4>{props.recipes[1].chef}</h4>
+            <img src={props.recipes[1].image_url} />
+          </div>
+          <div>
+            <h2>{props.recipes[2].title}</h2>
+            <h4>{props.recipes[2].chef}</h4>
+            <img src={props.recipes[2].image_url} />
+          </div>
+        </div>
     </div>
   );
 }
@@ -54,14 +59,35 @@ function Footer() {
       <h4>© Copywrite 2022</h4>
     </div>
   );
-  
 }
 
 function Home() {
-  return(
+  const [recipes, setRecipes] = useState([
+    {
+      id: 1,
+      title: "Raw Eggs",
+      chef: "Peter Jang",
+      image_url: "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg",
+    },
+    {
+      id: 2,
+      title: "Mud Pie",
+      chef: "Jay Wengrow",
+      image_url:
+        "https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg",
+    },
+    {
+      id: 3,
+      title: "Pizza",
+      chef: "Jay Wengrow",
+      image_url:
+        "https://static.onecms.io/wp-content/uploads/sites/9/2021/06/15/mozzarella-pizza-margherita-FT-RECIPE0621.jpg",
+    },
+  ])
+  return (
     <div>
       <RecipesNew />
-      <RecipesIndex />
+      <RecipesIndex recipes={recipes} />
     </div>
   );
 }

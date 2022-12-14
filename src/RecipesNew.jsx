c23f9035ebc7701e36ae666b49a238c132e06719
amanda <amanda@actualize.co>
2022-12-14 14:23:57 -0600

    connects form to make a new recipe

--- src/RecipesNew.jsx
+++ src/RecipesNew.jsx
@@ -1,13 +1,39 @@
-export function RecipesNew() {
+import axios from "axios";
+
+export function RecipesNew(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("makin a recipe!");
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/recipes.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+      });
+  }
+
   return (
     <div id="recipes-new">
       <h1>New Recipe</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          title: <input type="text" />
+          What it called?: <input name="title" type="text" />
         </div>
         <div>
-          chef: <input type="text" />
+          chef: <input name="chef" type="text" />
+        </div>
+        <div>
+          How long???: <input name="prep_time" type="text" />
+        </div>
+        <div>
+          What go in?: <input name="ingredients" type="text" />
+        </div>
+        <div>
+          directions: <input name="directions" type="text" />
+        </div>
+        <div>
+          Put in a picture: <input name="image" type="text" />
         </div>
         <button type="submit">Create recipe</button>
       </form>

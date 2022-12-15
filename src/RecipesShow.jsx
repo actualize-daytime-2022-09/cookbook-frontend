71369f938790d493bd32a6dfd10b357558a2953a
amanda <amanda@actualize.co>
2022-12-15 14:02:42 -0600

    creates edit form on show modal

--- src/RecipesShow.jsx
+++ src/RecipesShow.jsx
@@ -1,4 +1,14 @@
+import axios from "axios"
 export function RecipesShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.patch(`http://localhost:3000/recipes/${props.recipe.id}.json`, params).then((response) => {
+      console.log(response.data);
+      event.target.reset();
+    })
+  }
+
   return (
     <div id="recipes-show">
         <h2>{props.recipe.title}</h2>
@@ -6,6 +16,17 @@ export function RecipesShow(props) {
         <p>Ingredients: {props.recipe.ingredients}</p>
         <p>Directions: {props.recipe.directions}</p>
         <p>Prep time: {props.recipe.prep_time}</p>
+
+        <h2>Edit this Recipe!</h2>
+        <form onSubmit={handleSubmit}>
+          <div>Title: <input defaultValue={props.recipe.title} name="title" type="text" /></div>
+          <div>chef: <input defaultValue={props.recipe.chef} name="chef" type="text" /></div>
+          <div>ingredients: <input defaultValue={props.recipe.ingredients} name="ingredients" type="text" /></div>
+          <div>directions: <input defaultValue={props.recipe.directions} name="directions" type="text" /></div>
+          <div>prep_time: <input defaultValue={props.recipe.prep_time} name="prep_time" type="text" /></div>
+          <div>image_url: <input defaultValue={props.recipe.image_url} name="image_url" type="text" /></div>
+          <button type="submit">Update</button>
+        </form>
     </div>
   )
 }
